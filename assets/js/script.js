667ee9c2d68f21b7ba3da5e20a7ba3bad8d79bdd
naatab <naata25@gmail.com>
2022-08-09 03:39:29 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  
+html{
+  scroll-behavior: smooth;
+}
+  
